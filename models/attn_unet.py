92bcc4bdac71dc48cda069375208cc3943acf494
LuisRobaina <lrobaina3@gatech.edu>
2023-07-29 19:30:04 +0000

    added experiment logs

--- models/attn_unet.py
+++ models/attn_unet.py
@@ -1,7 +1,11 @@
 import torch
+import numpy as np
 from torch import nn
 from torch.nn import functional as F
 from .shared_components import ConvBlock, TransposeConvBlock
+from argparse import ArgumentParser
+from fastmri.pl_modules.mri_module import MriModule
+
 
 class attention_gate(nn.Module):
     """
@@ -44,6 +48,7 @@ class attention_gate(nn.Module):
         alpha = self.psi(alpha)
         alpha = self.Sigmoid(alpha)
         return x * alpha
+
 
 class AttentionUnet(nn.Module):
     """
@@ -122,6 +127,8 @@ class AttentionUnet(nn.Module):
         stack = []
         output = image
 
+        post_attn_norm_diff = []
+
         # apply down-sampling layers
         for layer in self.down_sample_layers:
             output = layer(output)
@@ -136,7 +143,12 @@ class AttentionUnet(nn.Module):
             output = transpose_conv(output)
             
             #calculate attention using the output from previous layer and skip connection layer
-            downsample_layer = attention(output, downsample_layer)
+            
+            post_attn_downsample_layer = attention(output, downsample_layer)
+            post_attn_norm_diff.append(
+                post_attn_downsample_layer.norm().item() - downsample_layer.norm().item()
+            )
+            downsample_layer = post_attn_downsample_layer
             
             # reflect pad on the right/botton if needed to handle odd input dimensions
             padding = [0, 0, 0, 0]
@@ -150,4 +162,169 @@ class AttentionUnet(nn.Module):
             output = torch.cat([output, downsample_layer], dim=1)
             output = conv(output)
 
-        return output
+        return output, {
+            "mean_post_attn_norm_diff": np.mean(post_attn_norm_diff)
+        }
+
+
+class AttnUnetModule(MriModule):
+    """
+    Attention Unet training module.
+    """
+
+    def __init__(
+        self,
+        in_chans=1,
+        out_chans=1,
+        chans=32,
+        num_pool_layers=4,
+        drop_prob=0.0,
+        lr=0.001,
+        lr_step_size=40,
+        lr_gamma=0.1,
+        weight_decay=0.0,
+        **kwargs,
+    ):
+        """
+        Args:
+            in_chans (int, optional): Number of channels in the input to the
+                U-Net model. Defaults to 1.
+            out_chans (int, optional): Number of channels in the output to the
+                U-Net model. Defaults to 1.
+            chans (int, optional): Number of output channels of the first
+                convolution layer. Defaults to 32.
+            num_pool_layers (int, optional): Number of down-sampling and
+                up-sampling layers. Defaults to 4.
+            drop_prob (float, optional): Dropout probability. Defaults to 0.0.
+            lr (float, optional): Learning rate. Defaults to 0.001.
+            lr_step_size (int, optional): Learning rate step size. Defaults to
+                40.
+            lr_gamma (float, optional): Learning rate gamma decay. Defaults to
+                0.1.
+            weight_decay (float, optional): Parameter for penalizing weights
+                norm. Defaults to 0.0.
+        """
+        super().__init__(**kwargs)
+        self.save_hyperparameters()
+
+        self.in_chans = in_chans
+        self.out_chans = out_chans
+        self.chans = chans
+        self.num_pool_layers = num_pool_layers
+        self.drop_prob = drop_prob
+        self.lr = lr
+        self.lr_step_size = lr_step_size
+        self.lr_gamma = lr_gamma
+        self.weight_decay = weight_decay
+
+        self.attn_unet = AttentionUnet(
+            in_chans=self.in_chans,
+            out_chans=self.out_chans,
+            chans=self.chans,
+            num_pool_layers=self.num_pool_layers,
+            drop_prob=self.drop_prob,
+        )
+
+    def forward(self, image):
+        output, info = self.attn_unet(image.unsqueeze(1))
+        self.log("mean_post_attn_norm_diff", info["mean_post_attn_norm_diff"])
+        
+        return output.squeeze(1), info
+
+    def training_step(self, batch, batch_idx):
+        
+        output, info = self(batch.image)
+        
+        loss = F.l1_loss(output, batch.target)
+
+        self.log("loss", loss.detach())
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        output, info = self(batch.image)
+        mean = batch.mean.unsqueeze(1).unsqueeze(2)
+        std = batch.std.unsqueeze(1).unsqueeze(2)
+
+        return {
+            "batch_idx": batch_idx,
+            "fname": batch.fname,
+            "slice_num": batch.slice_num,
+            "max_value": batch.max_value,
+            "output": output * std + mean,
+            "target": batch.target * std + mean,
+            "val_loss": F.l1_loss(output, batch.target),
+        }
+
+    def test_step(self, batch, batch_idx):
+        output, info = self.forward(batch.image)
+        mean = batch.mean.unsqueeze(1).unsqueeze(2)
+        std = batch.std.unsqueeze(1).unsqueeze(2)
+
+        return {
+            "fname": batch.fname,
+            "slice": batch.slice_num,
+            "output": (output * std + mean).cpu().numpy(),
+        }
+
+    def configure_optimizers(self):
+        optim = torch.optim.RMSprop(
+            self.parameters(),
+            lr=self.lr,
+            weight_decay=self.weight_decay,
+        )
+        scheduler = torch.optim.lr_scheduler.StepLR(
+            optim, self.lr_step_size, self.lr_gamma
+        )
+
+        return [optim], [scheduler]
+
+    @staticmethod
+    def add_model_specific_args(parent_parser):  # pragma: no-cover
+        """
+        Define parameters that only apply to this model
+        """
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        parser = MriModule.add_model_specific_args(parser)
+
+        # network params
+        parser.add_argument(
+            "--in_chans", default=1, type=int, help="Number of U-Net input channels"
+        )
+        parser.add_argument(
+            "--out_chans", default=1, type=int, help="Number of U-Net output chanenls"
+        )
+        parser.add_argument(
+            "--chans", default=1, type=int, help="Number of top-level U-Net filters."
+        )
+        parser.add_argument(
+            "--num_pool_layers",
+            default=4,
+            type=int,
+            help="Number of U-Net pooling layers.",
+        )
+        parser.add_argument(
+            "--drop_prob", default=0.0, type=float, help="U-Net dropout probability"
+        )
+
+        # training params (opt)
+        parser.add_argument(
+            "--lr", default=0.001, type=float, help="RMSProp learning rate"
+        )
+        parser.add_argument(
+            "--lr_step_size",
+            default=40,
+            type=int,
+            help="Epoch at which to decrease step size",
+        )
+        parser.add_argument(
+            "--lr_gamma", default=0.1, type=float, help="Amount to decrease step size"
+        )
+        parser.add_argument(
+            "--weight_decay",
+            default=0.0,
+            type=float,
+            help="Strength of weight decay regularization",
+        )
+
+        return parser
---
+++ scripts/train_attn_unet.py
@@ -0,0 +1,123 @@
+from pathlib import Path
+import pytorch_lightning as pl
+from fastmri.data.subsample import create_mask_for_mask_type
+from fastmri.data.transforms import UnetDataTransform
+from fastmri.pl_modules import FastMriDataModule
+
+import torch
+from torch.nn import functional as F
+
+import numpy as np
+import pytorch_lightning as pl
+from torch import nn
+from torchmetrics.metric import Metric
+
+import fastmri
+from fastmri.models.unet import ConvBlock, TransposeConvBlock
+from fastmri import evaluate
+from fastmri.pl_modules import MriModule
+from fastmri.pl_modules.mri_module import DistributedMetricSum
+
+from models.attn_unet import AttentionUnet, AttnUnetModule
+
+def train():
+
+    mask_types = [
+        "random",
+        "equispaced",
+        "equispaced_fraction",
+        "magic",
+        "magic_fraction"
+    ]
+
+    mask_type = mask_types[0]
+
+    # Number of center lines to use in mask
+    center_fractions = [0.09]
+
+    # acceleration rates to use for masks
+    accelerations = [4]
+
+    mask = create_mask_for_mask_type(
+        mask_type, center_fractions, accelerations
+    )
+
+    # Data specific Parameters
+    data_path = Path('data/')
+    test_path = Path('data/singlecoil_test')
+    challenge = "singlecoil"
+    test_split = "test"
+
+    sample_rate = 0.5
+    val_sample_rate = None
+    test_sample_rate = None
+    volume_sample_rate = None
+    val_volume_sample_rate = None
+    test_volume_sample_rate = None
+    use_dataset_cache_file = True
+    combine_train_val = False
+
+    # data loader arguments
+    batch_size = 32
+    num_workers = 60
+
+    train_transform = UnetDataTransform(challenge, mask_func=mask, use_seed=False)
+    val_transform = UnetDataTransform(challenge, mask_func=mask)
+    test_transform = UnetDataTransform(challenge)
+
+    data_module = FastMriDataModule(
+        data_path=data_path,
+        challenge=challenge,
+        train_transform=train_transform,
+        val_transform=val_transform,
+        test_transform=test_transform,
+        test_split=test_split,
+        test_path=test_path,
+        sample_rate=sample_rate,
+        distributed_sampler=None,
+        num_workers=num_workers,
+        batch_size=batch_size,
+    )
+
+    ########################################
+    # Attention UNet Model Hyperparameters #
+    ########################################
+
+    in_chans=1          # number of input channels to U-Net
+    out_chans=1         # number of output chanenls to U-Net
+    chans=32            # number of top-level U-Net channels
+    num_pool_layers=4   # number of U-Net pooling layers
+    drop_prob=0.4       # dropout probability
+    lr=0.001            # RMSProp learning rate
+    lr_step_size=40     # epoch at which to decrease learning rate
+    lr_gamma=0.1        # extent to which to decrease learning rate
+    weight_decay=0.0    # weight decay regularization strength
+
+    attn_unet_model = AttnUnetModule(
+        in_chans=in_chans,
+        out_chans=out_chans,
+        chans=chans,
+        num_pool_layers=num_pool_layers,
+        drop_prob=drop_prob,
+        lr=lr,
+        lr_step_size=lr_step_size,
+        lr_gamma=lr_gamma,
+        weight_decay=weight_decay,
+    )
+
+    from pytorch_lightning.loggers import TensorBoardLogger
+
+    trainer_config = dict(
+        accelerator = "gpu",
+        devices=1,                      # number of gpus to use
+        deterministic=False,            # makes things slower, but deterministic
+        default_root_dir='../logs',     # directory for logs and checkpoints
+        max_epochs=50,                  # max number of epochs
+        logger = TensorBoardLogger('logs/', name='attn_unet')
+    )
+
+    trainer = pl.Trainer(**trainer_config)
+    trainer.fit(attn_unet_model, datamodule=data_module)
+
+if __name__=="__main__":
+    train()
